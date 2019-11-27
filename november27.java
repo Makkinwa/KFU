024fd5c59ba282c937f553954c5b0903525cf796
Makinwa <51528406+Makkinwa@users.noreply.github.com>
2019-11-27 12:39:51 +0300

    Create november27.java

---
+++ november27.java
@@ -0,0 +1,71 @@
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private String LastName;
+    private char sex;
+    private LocalDate date;
+
+    public Runner(String name, String lastName, LocalDate date, char sex) {
+        this.name = name;
+        this.LastName = lastName;
+        this.date = date;
+        this.sex = sex;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        if (sex=='m' | sex=='f'){
+            this.sex = sex;
+        }
+        else {
+            System.out.println("This name is not allowed");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Runner{" +
+                "name='" + name + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", Birthdate='" + Birthdate + '\'' +
+                ", sex=" + sex +
+                '}';
+    }
+
+    public int getYears(){
+        return LocalDate.now().getYear() - date.getYear();
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Поехали!");
+
+    }
+}
